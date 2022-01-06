9b343d8f4375cf0ea77115f315fc9ffc37082233
Weebn't Programmer <89193581+RzCodez@users.noreply.github.com>
2022-01-06 14:33:20 +0800

    Add files via upload

--- ramadityo/assets/js/parallax-mouse.js
+++ ramadityo/assets/js/parallax-mouse.js
@@ -1,11 +1,94 @@
-document.addEventListener('mousemove', parallax);
-function parallax(e) {
-    this.querySelectorAll('#parallax').forEach(layer =>{
-        const speed =layer.getAttribute('data-speed');
-
-        const x = (window.innerWidth - e.pageX*speed) / 100;
-        const y = (window.innerHeight - e.pageY*speed) / 100;
-
-        layer.style.transform = 'translateX(${x}px) translateY(${y}px)';
-    })
-}
+$(function(){
+    $('.dotted-pattern').each(function() {
+      var off = $(this).offset().top
+      $(this).data('orig-offset', off);
+    });
+    $(window).scroll(function(){
+      var scrollTop = $(window).scrollTop();
+  
+       $('.dotted-pattern').each(function(){
+        var off = $(this).data('orig-offset');
+        
+         
+        if (scrollTop >= off) {
+          var translate =  (scrollTop - off) / $(window).height() * 30;
+          console.log(translate);
+          $(this).css({transform: 'translateX(' + translate +'%)'});
+        }
+        if (scrollTop <= off) {
+            var translate =  (scrollTop - off) / $(window).height() * 30;
+            console.log(translate);
+            $(this).css({transform: 'translateX(' + translate +'%)'});
+          }
+       });
+    });
+    $('.dotted-pattern2').each(function() {
+        var off = $(this).offset().top
+        $(this).data('orig-offset', off);
+      });
+      $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+    
+         $('.dotted-pattern2').each(function(){
+          var off = $(this).data('orig-offset');
+          
+           
+          if (scrollTop >= off) {
+            var translate =  (scrollTop - off) / $(window).height() * 30;
+            console.log(translate);
+            $(this).css({transform: 'translateY(' + translate +'%)'});
+          }
+          if (scrollTop <= off) {
+              var translate =  (scrollTop - off) / $(window).height() * 30;
+              console.log(translate);
+              $(this).css({transform: 'translateY(' + translate +'%)'});
+            }
+         });
+      });
+      $('.line-svg').each(function() {
+        var off = $(this).offset().top
+        $(this).data('orig-offset', off);
+      });
+      $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+    
+         $('.line-svg').each(function(){
+          var off = $(this).data('orig-offset');
+          
+           
+          if (scrollTop >= off) {
+            var translate =  (scrollTop - off) / $(window).height() * 30;
+            console.log(translate);
+            $(this).css({transform: 'translateY(' + translate +'%)'});
+          }
+          if (scrollTop <= off) {
+              var translate =  (scrollTop - off) / $(window).height() * 30;
+              console.log(translate);
+              $(this).css({transform: 'translateY(' + translate +'%)'});
+            }
+         });
+      });
+      $('.line-svg2').each(function() {
+        var off = $(this).offset().top
+        $(this).data('orig-offset', off);
+      });
+      $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+    
+         $('.line-svg2').each(function(){
+          var off = $(this).data('orig-offset');
+          
+           
+          if (scrollTop >= off) {
+            var translate =  (scrollTop - off) / $(window).height() * 30;
+            console.log(translate);
+            $(this).css({transform: 'translateY(' + translate +'%)'});
+          }
+          if (scrollTop <= off) {
+              var translate =  (scrollTop - off) / $(window).height() * 30;
+              console.log(translate);
+              $(this).css({transform: 'translateY(' + translate +'%)'});
+            }
+         });
+      });
+  });
