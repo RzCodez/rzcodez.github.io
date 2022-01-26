a41fda3503e626b84810a546fdd54856f5da0dfc
Weebn't Programmer <89193581+RzCodez@users.noreply.github.com>
2022-01-26 20:33:49 +0800

    Delete ramadityo directory

--- ramadityo/assets/js/scroll-detector.js
+++
@@ -1,6 +0,0 @@
-var scrollvalue = document.getElementById('scroll-detector');
-window.addEventListener('scroll', (e) => {
-    let scroll = this.scrollY;
-    console.log(scrollY);
-    // scrollvalue.innerHTML = 'Scroll Value' + scroll + 'px';
-})
