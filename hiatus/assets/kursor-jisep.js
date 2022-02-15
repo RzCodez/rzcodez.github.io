9343e4fa37d59cec5ecd7490156267fcfd516d6c
Weebn't Programmer <89193581+RzCodez@users.noreply.github.com>
2022-02-15 16:25:47 +0800

    add lagi

--- hiatus/assets/kursor-jisep.js
+++ hiatus/assets/kursor-jisep.js
@@ -2,66 +2,65 @@ var cursor = $(".cursor"),
     follower = $(".cursor-follower"),
     follower2 = $(".cursor-follower2"),
     image = $(".image");
-
-var posX = 0,
-    posY = 0;
-
-var mouseX = 0,
-    mouseY = 0;
-
-TweenMax.to({}, 0.016, {
-  repeat: -1,
-  onRepeat: function() {
-    posX += (mouseX - posX) / 9;
-    posY += (mouseY - posY) / 9;
-    
-    TweenMax.set(follower, {
-        css: {    
-        left: posX - 15,
-        top: posY - 14
-        }
-    });
-      
-    TweenMax.set(follower2, {
-        css: {    
-        left: posX - 12,
-        top: posY - 12
-        },
-        delay: .03,
-        ease: "Expo.easeOut"
-    });
- 
-    TweenMax.set(cursor, {
-        css: {    
-        left: mouseX,
-        top: mouseY
-        }
-    });
-  }
+TweenMax.set(".cursor-follower", {
+  xPercent: -50,
+  yPercent: -50
+});
+TweenMax.set(".cursor-follower2", {
+    xPercent: -50,
+    yPercent: -50
+});
+TweenMax.set("img", {
+    xPercent: -90,
+    yPercent: 20
 });
 
-$(document).on("mousemove", function(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
+window.addEventListener("mousemove", moveCircle);
 
+function moveCircle(e) {
+  TweenMax.to(".cursor-follower", 0.3, {
+    stagger: .3,
+    x: e.clientX,
+    y: e.clientY
+  });
+  TweenMax.to(".cursor-follower2", 0.4, {
+    x: e.clientX,
+    y: e.clientY,
+    ease: "power4.easeOut"
+  });
+  TweenMax.to("img", 0.4, {
+    x: e.clientX,
+    y: e.clientY,
+    ease: "power4.easeOut"
+  });
+}
+
+var cursorAnime = new TimelineMax()
+    .from(".cursor-follower2",{ 
+        duration: 0.3,
+        scale: 1,
+        ease: "Expo.inOut",
+    })
+    .reverse();
 $(".word").on("mouseenter", function(e) {
-    $(".cursor-follower2").addClass("bigger border-none");
+    $(".cursor-follower2").addClass("border-none");
+    cursorAnime.reversed(!cursorAnime.reversed());
     $(".cursor-follower").addClass("opacity-none");
 })
 
 $(".word").on("mouseleave", function(e) {
-    $(".cursor-follower2").removeClass("bigger border-none");
+    $(".cursor-follower2").removeClass("border-none");
+    cursorAnime.reversed(!cursorAnime.reversed());
     $(".cursor-follower").removeClass("opacity-none");
 })
 
 $(".container-word").on("mouseenter", function(e) {
-    $(".cursor-follower2").addClass("bigger border-none");
+    $(".cursor-follower2").addClass("border-none");
     $(".cursor-follower").addClass("opacity-none");
 })
 
 $(".container-word").on("mouseleave", function(e) {
-    $(".cursor-follower2").removeClass("bigger border-none");
+    $(".cursor-follower2").removeClass("border-none");
     $(".cursor-follower").removeClass("opacity-none");
 })
 
@@ -72,4 +71,23 @@ $(".exclution").on("mouseenter", function(e) {
 $(".exclution").on("mouseleave", function(e) {
     $(".cursor-follower2").removeClass("border-none");
     $(".cursor-follower").removeClass("opacity-none");
+})
+
+$(".link1").on("mouseenter", function(e) {
+    $(".image-2").addClass("opacity-full");
+})
+$(".link1").on("mouseleave", function(e) {
+    $(".image-2").removeClass("opacity-full");
+})
+$(".link2").on("mouseenter", function(e) {
+    $(".image-1").addClass("opacity-full");
+})
+$(".link2").on("mouseleave", function(e) {
+    $(".image-1").removeClass("opacity-full");
+})
+$(".link3").on("mouseenter", function(e) {
+    $(".image-3").addClass("opacity-full");
+})
+$(".link3").on("mouseleave", function(e) {
+    $(".image-3").removeClass("opacity-full");
 })
